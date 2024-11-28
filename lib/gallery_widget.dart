2e4363bf7a52d3623c10503b1f2848a6830f6fa7
Beyz61 <6trabzn1@gmail.com>
2024-11-28 23:55:03 +0100

    Added AppBar

--- lib/gallery_widget.dart
+++ lib/gallery_widget.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
 class GalleryWidget extends StatelessWidget {
-  const GalleryWidget({super.key});
+  const GalleryWidget({
+    super.key,
+  });
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "galerie",
-        style: TextStyle(
-          color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+          title: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("My Gallery",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.bold,
+                    fontStyle:  FontStyle.italic,
+                     shadows:  [
+                  Shadow(
+                    blurRadius: 0.1,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    offset: Offset(0.5, 0.5),
+                  ),
+                ],),
+          
+          ),
+          ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(180, 35, 36, 37),
+              Color.fromARGB(99, 85, 104, 118),
+            ],
+          ),
         ),
       ),
     );
